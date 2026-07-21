23795b507c2558d813ac23eed0073d03c884d7f4
= <=>
2026-07-21 19:29:24 -0300

    capitulo 01

---
+++ mysite/config/settings/development.py
@@ -0,0 +1,11 @@
+from .settings import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+SECRET_KEY = 'django-insecure-e2zj!rh5nmim3dvy($x@ks#9pa&e8+usyfedyqx87s-6sla+0r'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db_development.sqlite3'
+    }
+}
